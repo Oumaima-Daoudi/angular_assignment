873e5f8ff7085f7d70a7ff8e0928940e29ae72fc
oumaima <oumaimadaoudi13@gmail.com>
2023-10-23 15:37:20 +0100

    Define the Effects and  define the interface for the Store .

---
+++ src/store/store.ts
@@ -0,0 +1,17 @@
+import { CoffeeEffects } from "./effects";
+import { CoffeeState, coffeeReducer } from "./reducers";
+import { ActionReducer, Action } from "@ngrx/store";
+
+export interface AppState {
+  coffee: CoffeeState
+}
+
+export interface AppStore {
+  coffee: ActionReducer<CoffeeState, Action>;
+}
+
+export const appStore: AppStore = {
+  coffee: coffeeReducer
+}
+
+export const appEffects = [CoffeeEffects];
